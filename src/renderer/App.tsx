b62a7bb3065415db9a2d98e930a10e618381625e
Chu-son <chuson.dev@gmail.com>
2024-12-31 16:55:56 +0900

    Add drag-and-drop functionality with flowchart support

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,17 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
+import Flowchart from './Flowchart';
+
 function Hello() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="Hello">
@@ -10,30 +19,7 @@ function Hello() {
       </div>
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+        <button onClick={() => navigate('/flowchart')}>Flowchart</button>
       </div>
     </div>
   );
@@ -44,6 +30,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Hello />} />
+        <Route path="/flowchart" element={<Flowchart />} />
       </Routes>
     </Router>
   );
